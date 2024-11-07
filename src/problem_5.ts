bc5d795810e46d5308391c5bd39f1c98b9dc3420
Sabbir Hossain <sabbirh8181@gmail.com>
2024-11-08 03:00:27 +0600

    solved in the proble 5

---
+++ src/problem_5.ts
@@ -0,0 +1,17 @@
+// Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.
+
+type person ={
+    name: string;
+    age: number;
+}
+
+function getProperty<T, K extends keyof T>(Obj: T, key: K): T[K] {
+
+    return Obj[key];
+    
+}
+
+// Sample Input:
+const person = { name: "Alice", age: 30 };
+
+console.log(getProperty(person, "name"));
